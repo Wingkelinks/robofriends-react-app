b0c3c2df27915b0217c47fc52cef076781912af7
Wingkelinks <whitneyturner@Whitneys-MBP.gateway>
2019-10-06 10:22:10 +1000

    Updates

---
+++ precache-manifest.1ef9b219da3a81b45b1c2bff7a324b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b43b6ad669b113d4fa8c54b53c095a4",
+    "url": "/robofriends-react-app/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends-react-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6d56ccc46359783edb80",
+    "url": "/robofriends-react-app/static/css/main.4cc335e3.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends-react-app/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "6d56ccc46359783edb80",
+    "url": "/robofriends-react-app/static/js/main.7ef3ad28.chunk.js"
+  },
+  {
+    "revision": "0436146a132dc8fc2255",
+    "url": "/robofriends-react-app/static/js/runtime~main.5a02f2f2.js"
+  }
+]);
